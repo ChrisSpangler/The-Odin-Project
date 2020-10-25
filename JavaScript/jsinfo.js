a2d8e35c18c07e1b936749e2a86ebefede8ef3fd
ChrisFisqly <69488821+ChrisFisqly@users.noreply.github.com>
2020-10-25 09:37:20 -0700

    halfs

--- JavaScript/jsinfo.js
+++ JavaScript/jsinfo.js
@@ -22,3 +22,6 @@ alert( 8 % 3 ); // 2, a remainder of 8 divided by 3
 alert( 2 ** 2 ); // 4  (2 multiplied by itself 2 times)
 alert( 2 ** 3 ); // 8  (2 * 2 * 2, 3 times)
 alert( 2 ** 4 ); // 16 (2 * 2 * 2 * 2, 4 times)
+
+alert( 4 ** (1/2) ); // 2 (power of 1/2 is the same as a square root)
+alert( 8 ** (1/3) ); // 2 (power of 1/3 is the same as a cubic root)
