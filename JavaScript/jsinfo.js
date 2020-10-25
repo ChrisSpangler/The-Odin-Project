35a2e19d9d21837a7c7609f1b5260855b48f2cf5
ChrisFisqly <69488821+ChrisFisqly@users.noreply.github.com>
2020-10-25 09:36:45 -0700

    remainder

---
+++ JavaScript/jsinfo.js
@@ -0,0 +1,20 @@
+let x = 1;
+
+x = -x;
+alert( x ); // -1, unary negation was applied
+
+let x = 1, y = 3;
+alert( y - x ); // 2, binary minus subtracts values
+
+/* Maths
+The following math operations are supported:
+
+Addition +,
+Subtraction -,
+Multiplication *,
+Division /,
+Remainder %,
+Exponentiation **. */
+
+alert( 5 % 2 ); // 1, a remainder of 5 divided by 2
+alert( 8 % 3 ); // 2, a remainder of 8 divided by 3
