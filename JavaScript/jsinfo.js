fcade5dfe2a73cf53261a0f9ca07a3ac9e848055
ChrisFisqly <69488821+ChrisFisqly@users.noreply.github.com>
2020-10-26 06:13:50 -0700

    string

--- JavaScript/jsinfo.js
+++ JavaScript/jsinfo.js
@@ -25,3 +25,8 @@ alert( 2 ** 4 ); // 16 (2 * 2 * 2 * 2, 4 times)
 
 alert( 4 ** (1/2) ); // 2 (power of 1/2 is the same as a square root)
 alert( 8 ** (1/3) ); // 2 (power of 1/3 is the same as a cubic root)
+
+// String concatenation w/ binary
+
+let s = "my" + "string";
+alert(s); // mystring
